6b9f2604fa0c855ad6370db03234526bad1fda5f
AaqibQodeit <118366678+AaqibQodeit@users.noreply.github.com>
2023-03-20 23:10:04 +0530

    250 done

--- Datasetcreation.py
+++ Datasetcreation.py
@@ -5,14 +5,14 @@ os.makedirs("folder1")
 os.makedirs("folder2")
 
 # create unique files in folder1
-for i in range(1, 51):
+for i in range(1, 251):
     filename = f"file_{i}.txt"
     with open(f"folder1/{filename}", "w") as f:
         f.write(f"This is file {i} in folder1")
 
 # create identical files in folder1 and folder2
-for i in range(1, 51):
-    filename = f"file_{i + 50}.txt"
+for i in range(1, 251):
+    filename = f"file_{i + 250}.txt"
     content = f"This is file {i} in both folders"
     with open(f"folder1/{filename}", "w") as f1, open(f"folder2/{filename}", "w") as f2:
         f1.write(content)
@@ -20,6 +20,6 @@ for i in range(1, 51):
 
 # create unique files in folder2
 for i in range(1, 51):
-    filename = f"file_{i + 100}.txt"
+    filename = f"file_{i + 300}.txt"
     with open(f"folder2/{filename}", "w") as f:
         f.write(f"This is file {i} in folder2")
